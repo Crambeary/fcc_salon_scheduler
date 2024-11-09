31c2e90eadfb5782a52410ab7993e1ec205e4c63
Marc <marctotaro94@gmail.com>
2024-11-08 21:12:58 -0600

    Outlined script

--- salon.sh
+++ salon.sh
@@ -4,3 +4,19 @@
 PSQL="psql --username=marct --dbname=salon -c"
 
 echo -e "\n~~~~~ MY SALON ~~~~~"
+
+echo "Welcome to My Salon, how can I help you?"
+
+SERVICE_MENU() {
+  # get available services
+  # display services
+  # ask for service
+  # if not a real selection, error and retry
+  # get customer info
+  # if customer doesn't exist
+  # get customer name
+  # insert new customer
+  # get appointment time
+  # get customer id
+  # insert appointment
+}
